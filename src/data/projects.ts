c761684e45bf193c2ef88796a13c0257826ee27a
Gaurav Sobti <gauravsobti1@gmail.com>
2024-02-02 13:46:46 +1100

    Added 2 projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -31,6 +31,33 @@ const projects: ProjectType[] = [
     skills: ["strapi", "cms", "api", "aws"],
     codeLink: "https://github.com/gauravsoti1/sanjeevani-cms",
   },
+  {
+    video: "/redux-persist-authentication.webm",
+    title: "Redux Persist Authentication",
+    description:
+      "A project which makes use of redux persist to mimic localStorage as database and session storage for authentication",
+    skills: ["react", "typescript", "redux-toolkit", "redux-persist"],
+    codeLink:
+      "https://github.com/gauravsoti1/redux-persist-authentication-example",
+  },
+  {
+    video: "/dsalgomaster.webm",
+    title: "Datastructures and Algorithms",
+    description:
+      "All the notes and practice of data structures and algorithms from different sources like books, udemy. Also, maths from Euler",
+    skills: [
+      "data structures",
+      "algorithms",
+      "javascript",
+      "euler",
+      "leetcode",
+      "udemy",
+      "cracking the coding interview",
+      "elements of programming",
+      "freecodecamp",
+    ],
+    codeLink: "https://github.com/gauravsoti1/dsalgomaster",
+  },
 ];
 
 export default projects;
